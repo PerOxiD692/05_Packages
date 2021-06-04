2920e9b4988d14a898eb6d305edca607884cb817
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-06-04 12:34:24 +0200

    Package // Import // Call

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+import com.cc.java.*;
+
 public class App {
     public static void main(String[] args) {
        // toDo ...
+
+        //System.out.println("hi");
+        Test test = new Test();
+        test.sayHi();
+        // com.cc.java.Test test1 = new Test();
+        // test1.sayHi();
     }
 }
